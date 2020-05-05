c128cf6b49d67e4bd2b7899b0bd903fd272113d0
sarah <sarahthamer25@outlook.com>
2020-05-05 19:18:01 +0300

    Initial Commit

---
+++ hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw2
+//
+//  Created by sarah alshammari on 5/5/20.
+//  Copyright © 2020 sarah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
